dabd93df6b08ae0e3bcd565825528f4872c33540
wangli <468685628@qq.com>
2021-08-11 20:25:22 +0800

    initial commit

---
+++ springsecurity-01-webconfig/src/main/java/com/kuang/Springsecurity01WebconfigApplication.java
@@ -0,0 +1,13 @@
+package com.kuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecurity01WebconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springsecurity01WebconfigApplication.class, args);
+    }
+
+}
